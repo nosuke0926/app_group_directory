c310080e33116b48836d12e5ade80703896e674c
The one with the braid <braid@famedly.com>
2023-05-31 10:40:47 +0200

    chore: remove unused Android files
    - remove Android plugin files since not supported
    - migrate Flutter best practices
    - bump version
    
    Signed-off-by: The one with the braid <braid@famedly.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+# 2.1.0
+
+- Remove unused Android files
+- Update to current Flutter best practices
+
 # 2.0.0
 
 - Initial fork null-safe version.
--- example/android/app/src/main/java/me/wolszon/app_groups_directory_example/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package me.wolszon.app_group_directory_example;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
